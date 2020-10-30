e6bfd43a8377332560123c7a94cdcf0de5413e76
Hanif Rimamtomo <hanifsatrio12@gmail.com>
2020-10-30 15:42:33 +0700

    Belajar PHP

---
+++ E41191458_HanifSatrioRimamtomo/Belajar_PHP/sintax_html_php.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo "Belajar PHP"?></title>
+</head>
+<body>
+	<?php
+		echo "saya sedang belajar php<br>";
+		echo "<p>Belajar PHP hingga master</p>";
+	?>
+</body>
+</html>
