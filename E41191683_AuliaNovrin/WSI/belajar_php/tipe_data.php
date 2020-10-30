5d79ddc7520fbccd040d3901e73a74108c2b742b
aulianovrin <novrinjr@gmail.com>
2020-10-30 14:22:59 +0700

    belajarphp

---
+++ E41191683_AuliaNovrin/WSI/belajar_php/tipe_data.php
@@ -0,0 +1,27 @@
+<?php
+
+//tipe data char (karakter)
+$jenis_kelamin = "L";
+
+//tipe  data string(teks)
+$nama_lengkap = "Aulia Novrin The Best Rusher";
+
+//tipe data integer
+$umur = 20;
+
+//tipe data float
+$berat = 61;
+
+// tipe data float
+$tinggi = 167;
+
+//tipe data boolean
+$menikah = false;
+
+echo "Name: $nama_lengkap<br>";
+echo "Gender: $jenis_kelamin<br>";
+echo "Age: $umur tahun<br>";
+echo "Weigth: $berat kg<br>";
+echo "Heigth: $tinggi cm<br>";
+echo "Menikah: $menikah";
+?>
