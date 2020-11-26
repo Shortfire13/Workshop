13792c909eee9bfceebd3761cd2bc750842eb1dd
Hanif Rimamtomo <hanifsatrio12@gmail.com>
2020-11-26 20:07:34 +0700

    Tugas_Crud

---
+++ E41191458_HanifSatrioRimamtomo/Belajar.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "HAEE<br>";
+echo "Belajar PHP<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+</body>
+</html>
