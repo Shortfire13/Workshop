ea1dcfec77cbd15de8fe248894a1eb7e1b816bcd
geofannyher <geofannyher@gmail.com>
2021-01-14 12:55:53 +0700

    update

--- SiSegar/updateprofile.php
+++ SiSegar/updateprofile.php
@@ -6,14 +6,15 @@
                     $nohp = $_POST['nohp'];
                     $alamat = $_POST['alamat'];
                     $gambar = $_FILES['gambar']['name'];
+                    $id = $_POST['id'];
                     
                     if (empty($gambar)) {
                         //tidak ada gambar
-                        $update = mysqli_query($koneksi,"UPDATE user SET nama_user='$nama',no_tlp='$nohp',alamat='$alamat' WHERE id_user='4'");
+                        $update = mysqli_query($koneksi,"UPDATE user SET nama_user='$nama',no_tlp='$nohp',alamat='$alamat' WHERE id_user='$id'");
                        header('location:profile.php');
                     }else{
                         //ada gambar
-                             $update = mysqli_query($koneksi,"UPDATE user SET nama_user='$nama',no_tlp='$nohp',alamat='$alamat',gambar='$gambar' WHERE id_user='4'");
+                             $update = mysqli_query($koneksi,"UPDATE user SET nama_user='$nama',no_tlp='$nohp',alamat='$alamat',gambar='$gambar' WHERE id_user='$id'");
                        header('location:profile.php');
                     }
                 }
